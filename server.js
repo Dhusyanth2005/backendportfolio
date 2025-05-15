fcfa4d8a7450c1511300442b1a17b4114a8bd0f0
Dhusyanth2005 <sdhusyanth20@gmail.com>
2025-05-15 21:47:42 +0530

    second commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const fileUpload = require('express-fileupload');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors('*'));
 app.use(express.json());
 app.use(fileUpload({
   useTempFiles: true,
